5a04e78920d6ee0d14185ac63a937a34ba27fb83
Umigishi-Aoi <umigishi.aoi@gmail.com>
2021-12-13 11:06:33 +0900

    test separating the execution environment

--- tourou/lib/main.dart
+++ tourou/lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  static const flavor = String.fromEnvironment('FLAVOR');
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -14,8 +15,14 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(title: Text("test"),),
-        body: Container(
-
+        body: Center(
+          child: Container(
+            child: Column(
+              children: [
+                Text("FLAVOR is $flavor")
+              ],
+            )
+          ),
         )
       )
     );
