0a5b0f98a81a7f40bf2d4e9239c452184d590dc3
Umigishi-Aoi <umigishi.aoi@gmail.com>
2021-12-16 09:39:26 +0900

    #4 implement atoms text

---
+++ tourou/lib/ui/atoms/custom_text.dart
@@ -0,0 +1,29 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Project imports:
+import 'package:tourou/gen/colors.gen.dart';
+
+class CustomText extends StatelessWidget {
+  CustomText(
+      {Key? key, required this.text, this.color, this.fontSize, this.bold})
+      : super(key: key);
+
+  final String text;
+  final Color? color;
+  final double? fontSize;
+  final FontWeight? bold;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Text(
+        text,
+        style: TextStyle(
+            color: color ?? ColorName.mainBlack,
+            fontSize: fontSize ?? 16,
+            fontWeight: bold ?? FontWeight.normal),
+      ),
+    );
+  }
+}
