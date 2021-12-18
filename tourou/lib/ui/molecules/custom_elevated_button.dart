e7b2fbfc5977556d678e45a263ecb793e9d8d1ec
Umigishi-Aoi <umigishi.aoi@gmail.com>
2021-12-18 22:43:35 +0900

    sort

--- tourou/lib/ui/molecules/custom_elevated_button.dart
+++ tourou/lib/ui/molecules/custom_elevated_button.dart
@@ -1,4 +1,7 @@
+// Flutter imports:
 import 'package:flutter/material.dart';
+
+// Project imports:
 import '../../gen/colors.gen.dart';
 import '../../res/constants.dart';
 import '../atoms/custom_text.dart';
