f7e00bdc9fe537130f8d106ac66de786278e132c
Umigishi-Aoi <umigishi.aoi@gmail.com>
2021-12-18 20:03:06 +0900

    edit main.dart by using enum

--- tourou/lib/res/constants.dart
+++ tourou/lib/res/constants.dart
@@ -1,2 +1,18 @@
 const double mainTextSize = 24;
 const int imageDuration = 1;
+const flavor = String.fromEnvironment('FLAVOR');
+
+enum Flavor{
+  dev,
+  stg,
+  prod,
+  undefined
+}
+
+Flavor makeFlavor(String flavor){
+  try{
+    return Flavor.values.byName(flavor);
+  }catch(e){
+    return Flavor.undefined;
+  }
+}
---
+++ tourou/lib/ui/molecules/custom_google_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomGoogleSignInButton extends StatelessWidget {
+  const CustomGoogleSignInButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
