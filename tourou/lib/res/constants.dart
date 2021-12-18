28febb4f67c4b377392eb3647819d01de7ad9058
Umigishi-Aoi <umigishi.aoi@gmail.com>
2021-12-18 20:07:45 +0900

    format and sort

--- tourou/lib/res/constants.dart
+++ tourou/lib/res/constants.dart
@@ -2,17 +2,12 @@ const double mainTextSize = 24;
 const int imageDuration = 1;
 const flavor = String.fromEnvironment('FLAVOR');
 
-enum Flavor{
-  dev,
-  stg,
-  prod,
-  undefined
-}
+enum Flavor { dev, stg, prod, undefined }
 
-Flavor makeFlavor(String flavor){
-  try{
+Flavor makeFlavor(String flavor) {
+  try {
     return Flavor.values.byName(flavor);
-  }catch(e){
+  } catch (e) {
     return Flavor.undefined;
   }
 }
