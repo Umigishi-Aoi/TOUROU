928265834c705baefad6ddfedaaf55d530a03d32
Umigishi-Aoi <umigishi.aoi@gmail.com>
2021-12-19 08:04:01 +0900

    #17 implement atoms custom icon

---
+++ tourou/lib/ui/atoms/custom_icon.dart
@@ -0,0 +1,27 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Project imports:
+import '../../gen/colors.gen.dart';
+
+class CustomIcon extends StatelessWidget {
+  final IconData iconData;
+  final double size;
+  final Color? color;
+
+  const CustomIcon({
+    Key? key,
+    required this.iconData,
+    required this.size,
+    this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      iconData,
+      size: size,
+      color: color ?? ColorName.textWhite,
+    );
+  }
+}
