acaa71793cad38c6cf93d12518be92564723b30d
Umigishi-Aoi <umigishi.aoi@gmail.com>
2021-12-19 16:53:16 +0900

    add custom Icon button

---
+++ tourou/lib/ui/molecules/custom_icon_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:tourou/ui/atoms/custom_icon.dart';
+
+class CustomIconButton extends StatelessWidget {
+  final void Function() function;
+
+  final IconData iconData;
+  final double size;
+  final Color? color;
+
+  const CustomIconButton(
+      {Key? key,
+      required this.function,
+      required this.iconData,
+      required this.size,
+      this.color})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: function,
+      icon: CustomIcon(
+        iconData: iconData,
+        size: size,
+        color: color,
+      ),
+    );
+  }
+}
