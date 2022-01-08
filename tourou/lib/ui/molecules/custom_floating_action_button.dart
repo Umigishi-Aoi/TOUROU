874e2730745266cf84ae9ce9a9c6fb3303694412
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-01-08 21:34:11 +0900

    modify custom floating action button for atomic design

--- tourou/lib/ui/molecules/custom_floating_action_button.dart
+++ tourou/lib/ui/molecules/custom_floating_action_button.dart
@@ -2,36 +2,32 @@
 import 'package:flutter/material.dart';
 
 // Project imports:
-import '../../gen/colors.gen.dart';
-import '../../res/constants.dart';
 import '../atoms/custom_icon.dart';
 
 class CustomFloatingActionButton extends StatelessWidget {
   final void Function() function;
   final Color? buttonColor;
 
-  final IconData? iconData;
-  final double? iconSize;
+  final double iconSize;
   final Color? iconColor;
 
   const CustomFloatingActionButton(
       {Key? key,
       required this.function,
       this.buttonColor,
-      this.iconData,
-      this.iconSize,
+      required this.iconSize,
       this.iconColor})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      foregroundColor: buttonColor ?? ColorName.fABBackgoround,
+      foregroundColor: buttonColor,
       onPressed: function,
       child: CustomIcon(
-        iconData: iconData ?? Icons.add,
-        size: iconSize ?? fabIconSize,
-        color: iconColor ?? ColorName.textWhite,
+        iconData: Icons.add,
+        size: iconSize,
+        color: iconColor,
       ),
     );
   }
