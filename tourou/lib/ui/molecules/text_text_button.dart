0a4d026a1e657537ecab2203c17f694ca513ba92
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-01-09 15:04:21 +0900

    modify text text button for atomic design

--- tourou/lib/ui/molecules/text_text_button.dart
+++ tourou/lib/ui/molecules/text_text_button.dart
@@ -2,7 +2,6 @@
 import 'package:flutter/material.dart';
 
 // Project imports:
-import '../../gen/colors.gen.dart';
 import '../atoms/custom_text.dart';
 
 class TextTextButton extends StatelessWidget {
@@ -10,23 +9,25 @@ class TextTextButton extends StatelessWidget {
   final Color? firstColor;
   final double? fontSize;
   final FontWeight? firstBold;
+  final String? fontFamily;
 
   final String secondText;
   final Color? secondColor;
   final FontWeight? secondBold;
   final void Function()? function;
 
-  const TextTextButton(
-      {Key? key,
-      required this.firstText,
-      this.firstColor,
-      this.fontSize,
-      this.firstBold,
-      required this.secondText,
-      this.secondColor,
-      this.secondBold,
-      this.function})
-      : super(key: key);
+  const TextTextButton({
+    Key? key,
+    required this.firstText,
+    this.firstColor,
+    this.fontSize,
+    this.firstBold,
+    this.fontFamily,
+    required this.secondText,
+    this.secondColor,
+    this.secondBold,
+    this.function,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +42,7 @@ class TextTextButton extends StatelessWidget {
         ),
         CustomText(
           text: secondText,
-          color: secondColor ?? ColorName.textButton,
+          color: secondColor,
           fontSize: fontSize,
           bold: secondBold,
         )
