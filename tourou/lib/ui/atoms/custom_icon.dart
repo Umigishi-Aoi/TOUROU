2313310478b2b9cb8bd8e43e1204a6b7fb7d8e73
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-01-09 18:20:49 +0900

    modify null safety field in custom icon

--- tourou/lib/ui/atoms/custom_icon.dart
+++ tourou/lib/ui/atoms/custom_icon.dart
@@ -4,13 +4,13 @@ import 'package:flutter/material.dart';
 class CustomIcon extends StatelessWidget {
   final IconData iconData;
   final double size;
-  final Color? color;
+  final Color color;
 
   const CustomIcon({
     Key? key,
     required this.iconData,
     required this.size,
-    this.color,
+    required this.color,
   }) : super(key: key);
 
   @override
