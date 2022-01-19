35c3b56c4e4a12638c1594231e2e6e725edb1039
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-01-19 18:13:59 +0900

    add world tourou list

--- tourou/lib/ui/templates/world_tourou_template.dart
+++ tourou/lib/ui/templates/world_tourou_template.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:tourou/ui/atoms/tourou_tab_bar.dart';
+import 'package:tourou/ui/organisms/world_tourou_list.dart';
+
+import '../../ads/adaptive_banner_ad.dart';
 
 class WorldTourouTemplate extends StatelessWidget {
+  final bool isTest;
+
   const WorldTourouTemplate({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              TourouTabBar(
+                firstTabText: firstTabText,
+                secondTabText: secondTabText,
+                controller: controller,
+                labelColor: labelColor,
+                unselectedLabelColor: unselectedLabelColor,
+                indicatorWeight: indicatorWeight,
+                horizontalPadding: horizontalPadding,
+                verticalPadding: verticalPadding,),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    WorldTourouList(),
+                    WorldTourouList(),
+                  ],
+                ),
+              ),
+              AdaptiveBannerAd()
+            ],
+          ),
+        ),
+      ),
+    )
   }
 }
