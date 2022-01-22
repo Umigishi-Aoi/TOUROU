6c97de280f8251fbff0a03e76aa7e8d5ec0f51b4
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-01-22 16:46:03 +0900

    add world tourou page

---
+++ tourou/lib/ui/pages/world_tourou_page.dart
@@ -0,0 +1,30 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Project imports:
+import '../../temp/tourou_test_data.dart';
+import '../templates/world_tourou_template.dart';
+
+class WorldTourouPage extends StatelessWidget {
+  final bool isTest;
+
+  const WorldTourouPage({Key? key, required this.isTest}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WorldTourouTemplate(
+      isTest: isTest,
+      newWorldTourous: isTest
+          ? TourouTestData.testData().tourouTestData
+          : TourouTestData.testData().tourouTestData,
+      trendWorldTourous: isTest
+          ? TourouTestData.testData().tourouTestData
+          : TourouTestData.testData().tourouTestData,
+      worldTourouTapFunction: (tourouData) {},
+      reportFunction: (tourouData) {},
+      profileFunction: (tourouData) {},
+      goodButtonFunction: (tourouData) {},
+      tourouImageFunction: (tourouData) {},
+    );
+  }
+}
