a0a8788ef6a04545bd401cb3322655fe69b8e434
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-01-23 19:28:03 +0900

    edit comment

--- tourou/lib/ads/ad_helper_export.dart
+++ tourou/lib/ads/ad_helper_export.dart
@@ -1,4 +1,4 @@
+// ignore: no_such_file_or_directory
 export 'ad_helper_stub.dart'
     if (dart.library.test) 'ad_helper_test.dart'
-    // ignore: no_such_file_or_directory
     if (dart.library.io) 'ad_helper.dart';
