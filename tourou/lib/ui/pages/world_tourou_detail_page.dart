e2501d10b77b9b1836d401dc148dd1077da18c71
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-01-25 14:06:28 +0900

    implement world tourou detail page

--- tourou/lib/ui/pages/world_tourou_detail_page.dart
+++ tourou/lib/ui/pages/world_tourou_detail_page.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:tourou/temp/toruou_data.dart';
+import 'package:tourou/ui/templates/world_tourou_detail_template.dart';
 
 class WorldTourouDetailPage extends StatelessWidget {
-  const WorldTourouDetailPage({Key? key}) : super(key: key);
+  final TourouData worldTourou;
+
+  const WorldTourouDetailPage({Key? key, required this.worldTourou})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return WorldTourouDetailTemplate(
+      worldTourou: worldTourou,
+      reportFunction: (tourouData) {},
+      profileFunction: (tourouData) {},
+      goodButtonFunction: (tourouData) {},
+      tourouImageFunction: (tourouData) {},
+    );
   }
 }
