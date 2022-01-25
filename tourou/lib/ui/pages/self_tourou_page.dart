76a10a7f282c0831d1e905f1ab89d4e6e387f148
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-01-25 15:44:07 +0900

    add self tourou page

---
+++ tourou/lib/ui/pages/self_tourou_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SelfTourouPage extends StatelessWidget {
+  const SelfTourouPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
