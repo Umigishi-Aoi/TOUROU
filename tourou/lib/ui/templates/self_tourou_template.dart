6722d71a4414986f55bcff4531c3958b669957f1
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-01-25 15:10:08 +0900

    add self tourou template

---
+++ tourou/lib/ui/templates/self_tourou_template.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SelfTourouTemplate extends StatelessWidget {
+  const SelfTourouTemplate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
