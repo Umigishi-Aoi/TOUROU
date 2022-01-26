642e139aab1f644e31c2a21f05ee6fa07ae5fd25
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-01-26 16:18:13 +0900

    define custom floating action button

--- tourou/lib/ui/molecules/custom_floating_action_button.dart
+++ tourou/lib/ui/molecules/custom_floating_action_button.dart
@@ -1,32 +1,27 @@
-// Flutter imports:
 import 'package:flutter/material.dart';
 
-// Project imports:
 import '../atoms/custom_icon.dart';
 
 class CustomFloatingActionButton extends StatelessWidget {
-  final void Function() function;
-  final Color buttonColor;
+  final Color fabColor;
+  final void Function() fabFunction;
 
-  final double iconSize;
   final Color iconColor;
 
   const CustomFloatingActionButton(
       {Key? key,
-      required this.function,
-      required this.buttonColor,
-      required this.iconSize,
+      required this.fabColor,
+      required this.fabFunction,
       required this.iconColor})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      foregroundColor: buttonColor,
-      onPressed: function,
+      backgroundColor: fabColor,
+      onPressed: fabFunction,
       child: CustomIcon(
         iconData: Icons.add,
-        size: iconSize,
         color: iconColor,
       ),
     );
