bd2576e06e2af7cd2b29b5f3acfe158059308a7a
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-01-26 16:22:22 +0900

    import sort and format

--- tourou/lib/ui/molecules/custom_floating_action_button.dart
+++ tourou/lib/ui/molecules/custom_floating_action_button.dart
@@ -1,5 +1,7 @@
+// Flutter imports:
 import 'package:flutter/material.dart';
 
+// Project imports:
 import '../atoms/custom_icon.dart';
 
 class CustomFloatingActionButton extends StatelessWidget {
