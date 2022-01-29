61022c84ad0c4fd42c06d9686732496d3337085e
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-01-29 15:05:42 +0900

    modify new tourou template

--- tourou/lib/ui/templates/new_tourou_template.dart
+++ tourou/lib/ui/templates/new_tourou_template.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:tourou/gen/colors.gen.dart';
+import 'package:tourou/gen/fonts.gen.dart';
 import 'package:tourou/ui/atoms/custom_textfield.dart';
 import 'package:tourou/ui/molecules/custom_elevated_button.dart';
 
 class NewTourouTemplate extends StatelessWidget {
+  final TextEditingController controller;
+
   const NewTourouTemplate({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final double displayHeight = MediaQuery
+        .of(context)
+        .size
+        .height;
+    final double displayWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
     return Scaffold(
       appBar: AppBar(),
       body: Center(
@@ -19,21 +32,23 @@ class NewTourouTemplate extends StatelessWidget {
               width: width,
               minLines: minLines,
               fontSize: fontSize,
-              textColor: textColor,
-              fontFamily: fontFamily,
-              fillColor: fillColor,
-              borderColor: borderColor,
-              textInputFormatters: textInputFormatters,
+              textColor: ColorName.mainBlack,
+              fontFamily: FontFamily.mplus1,
+              fillColor: ColorName.tourouBackground,
+              borderColor: ColorName.tourouBackground,
+              textInputFormatters: [
+                FilteringTextInputFormatter.allow(RegExp(r'{,300}')),
+              ],
             ),
             CustomElevatedButton(
-                text: text,
-                color: color,
-                fontSize: fontSize,
-                fontFamily: fontFamily,
-                height: height,
-                width: width,
-                buttonColor: buttonColor,
-                function: function,
+              text: text,
+              color: ColorName.mainBlack,
+              fontSize: fontSize,
+              fontFamily: FontFamily.mplus1,
+              height: height,
+              width: width,
+              buttonColor: ColorName.tourouBackground,
+              function: function,
             )
           ],
         ),
