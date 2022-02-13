ab6ff076ad15457bc1d0f9e6a83b1dfd4bcd044f
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-02-13 20:03:07 +0900

    modify new tourou page

---
+++ tourou/lib/ui/pages/new_tourou_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewTourouPage extends StatelessWidget {
+  const NewTourouPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
