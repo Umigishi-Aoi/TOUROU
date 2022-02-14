70ec3aacc1dd0f9d35e2d1c1555eee08fa1eab29
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-02-14 15:45:09 +0900

    modify new tourou page

--- tourou/lib/ui/pages/new_tourou_page.dart
+++ tourou/lib/ui/pages/new_tourou_page.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:tourou/ui/templates/new_tourou_template.dart';
 
-class NewTourouPage extends StatelessWidget {
+class NewTourouPage extends StatefulWidget {
   const NewTourouPage({Key? key}) : super(key: key);
 
   @override
+  State<NewTourouPage> createState() => _NewTourouPageState();
+}
+
+class _NewTourouPageState extends State<NewTourouPage> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return NewTourouTemplate(
+      function: () {  },
+      controller: _controller,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
