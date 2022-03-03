c9513b210993bf88e50910580787343e50a21d7d
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-03-03 21:31:32 +0900

    add custom elavated button

--- tourou/lib/ui/templates/setting_template.dart
+++ tourou/lib/ui/templates/setting_template.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tourou/ui/molecules/custom_elevated_button.dart';
 
 class SettingTemplate extends StatelessWidget {
   const SettingTemplate({Key? key}) : super(key: key);
@@ -9,7 +10,9 @@ class SettingTemplate extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Column(
-            children: [],
+            children: [
+              CustomElevatedButton(text: , color: color, fontSize: fontSize, fontFamily: fontFamily, height: height, width: width, buttonColor: buttonColor, function: function)
+            ],
           ),
         ),
       ),
