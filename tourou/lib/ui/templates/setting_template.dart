808d64792c31601d425ccf3ed8e4a3fa420da267
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-03-07 16:23:26 +0900

    add function

--- tourou/lib/ui/templates/setting_template.dart
+++ tourou/lib/ui/templates/setting_template.dart
@@ -4,6 +4,9 @@ import 'package:tourou/gen/fonts.gen.dart';
 import 'package:tourou/ui/molecules/custom_elevated_button.dart';
 
 class SettingTemplate extends StatelessWidget {
+
+  final void Function() editProfileFunction;
+
   const SettingTemplate({Key? key}) : super(key: key);
 
   @override
@@ -13,7 +16,7 @@ class SettingTemplate extends StatelessWidget {
         child: Center(
           child: Column(
             children: [
-              CustomElevatedButton(text: , color: ColorName.mainBlack, fontSize: fontSize, fontFamily: FontFamily.mplus1, height: height, width: width, buttonColor: ColorName.tourouBackground, function: function,),
+              CustomElevatedButton(text: , color: ColorName.mainBlack, fontSize: fontSize, fontFamily: FontFamily.mplus1, height: height, width: width, buttonColor: ColorName.tourouBackground, function: editProfileFunction,),
             ],
           ),
         ),
