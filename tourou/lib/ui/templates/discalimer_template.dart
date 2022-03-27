b5f3b316d1db9180518905488d33a6d306fa73fb
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-03-27 16:13:01 +0900

    add disclaimer template

---
+++ tourou/lib/ui/templates/discalimer_template.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import '../../gen/colors.gen.dart';
+import '../../gen/fonts.gen.dart';
+import '../../l10n/app_localizations.dart';
+import '../../res/constants.dart';
+import '../atoms/custom_text.dart';
+
+class DiscalimerTemplate extends StatelessWidget {
+  final void Function() backFunction;
+
+  const DiscalimerTemplate({
+    Key? key,
+    required this.backFunction,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorName.mainBlack,
+      appBar: AppBar(
+        backgroundColor: ColorName.mainBlack,
+        title: CustomText(
+          text: AppLocalizations.of(context)!.disclaimer,
+          color: ColorName.textWhite,
+          fontSize: newTourouFontSize,
+          fontFamily: FontFamily.mplus1,
+        ),
+        leading: Center(
+          child: CustomText(
+            text: AppLocalizations.of(context)!.back,
+            color: ColorName.switchAccentColor,
+            fontSize: newTourouFontSize,
+            fontFamily: FontFamily.mplus1,
+            function: backFunction,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(sentencePadding),
+        child: CustomText(
+          color: ColorName.mainBlack, 
+          fontSize: mainTextFontSize, 
+          text: '',
+          fontFamily: FontFamily.mplus1,
+        ),
+      ),
+    );
+  }
+}
