12f9045376649b761cae9a535cf1978f788524eb
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-03-29 22:55:18 +0900

    test slack connection

---
+++ tourou/lib/navigator.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Navigator extends StatelessWidget {
+  const Navigator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
