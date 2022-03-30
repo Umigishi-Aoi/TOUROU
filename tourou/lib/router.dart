27baddbbece887d7b5ac0175d1545c52adff5a40
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-03-30 23:03:50 +0900

    set GoRoutes

--- tourou/lib/router.dart
+++ tourou/lib/router.dart
@@ -3,9 +3,98 @@ import 'package:go_router/go_router.dart';
 
 import 'ui/pages/pages.dart';
 
-final router = Provider(
-  (ref) => GoRouter(
+final router = Provider((ref) {
+  bool isTest = false;
+
+  return GoRouter(
     routes: [
+      GoRoute(
+        path: '/title',
+        builder: (context, state) => TitlePage(),
+      ),
+      GoRoute(
+        path: '/edit_profile',
+        builder: (context, state) => EditProfilePage(),
+      ),
+      GoRoute(
+        path: '/how_to_use',
+        builder: (context, state) => HowToUsePage(),
+      ),
+      GoRoute(
+        path: '/language_setting',
+        builder: (context, state) => LanguageSettingPage(),
+      ),
+      GoRoute(
+        path: '/new_profile_setting',
+        builder: (context, state) => NewUserRegistrationPage(),
+      ),
+      GoRoute(
+        path: '/new_registration',
+        builder: (context, state) => NewRegistrationPage(
+          isTest: isTest,
+        ),
+      ),
+      GoRoute(
+        path: '/new_tourou',
+        builder: (context, state) => NewTourouPage(),
+      ),
+      GoRoute(
+        path: '/privacy_policy',
+        builder: (context, state) => PrivacyPolicyPage(),
+      ),
+      GoRoute(
+        path: '/profile',
+        builder: (context, state) => ProfilePage(
+          isTest: isTest,
+        ),
+      ),
+      GoRoute(
+        path: '/self_tourou_detail',
+        builder: (context, state) => SelfTourouDetailPage(
+          selfTourou: null,
+        ),
+      ),
+      GoRoute(
+        path: '/self_tourou',
+        builder: (context, state) => SelfTourouPage(
+          isTest: isTest,
+        ),
+      ),
+      GoRoute(
+        path: '/setting',
+        builder: (context, state) => SettingPage(
+          isTest: isTest,
+        ),
+      ),
+      GoRoute(
+        path: '/sign_in',
+        builder: (context, state) => SignInPage(
+          isTest: isTest,
+        ),
+      ),
+      GoRoute(
+        path: '/sign_in',
+        builder: (context, state) => SignInPage(
+          isTest: isTest,
+        ),
+      ),
+      GoRoute(
+        path: '/title_page',
+        builder: (context, state) => TitlePage(
+        ),
+      ),
+      GoRoute(
+        path: '/world_tourou_detail',
+        builder: (context, state) => WorldTourouDetailPage(
+          worldTourou: null,
+        ),
+      ),
+      GoRoute(
+        path: '/world_tourou',
+        builder: (context, state) => WorldTourouPage(
+          isTest: isTest,
+        ),
+      ),
     ],
-  ),
-);
+  );
+});
