bde7825abc52b2e5902a977c97e7b8f9788aa680
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-04-15 10:14:33 +0900

    modify tourou_test_data.dart

--- tourou/lib/temp/tourou_test_data.dart
+++ tourou/lib/temp/tourou_test_data.dart
@@ -1,47 +1,58 @@
 // Project imports:
+// ignore_for_file: lines_longer_than_80_chars
+
 import '../gen/assets.gen.dart';
 import 'toruou_data.dart';
 
 class TourouTestData {
-  List<TourouData> tourouTestData;
-
-  TourouTestData._({required this.tourouTestData});
-
   factory TourouTestData() {
-    List<TourouData> testData = [];
+    final testData = <TourouData>[];
 
-    for (int i = 0; i < 25; i++) {
-      testData.add(TourouData(
-        profileImagePath: Assets.images.iconNoImage.path,
-        userName: 'kenta',
-        userId: 'kenta1234',
-        tourouText: '今日はいい天気ですね',
-        goodNumber: '0',
-      ));
-      testData.add(TourouData(
-        profileImagePath: Assets.images.iconNoImage.path,
-        userName: 'kentakentakentakenta',
-        userId: 'kenta5678',
-        tourouText: '今日はいい天気ですね',
-        goodNumber: '1',
-      ));
-      testData.add(TourouData(
-        profileImagePath: Assets.images.iconNoImage.path,
-        userName: 'kenta',
-        userId: 'kenta9012',
-        tourouText:
-            '今日はいい天気ですね今日はいい天気ですね今日はいい天気ですね今日はいい天気ですね今日はいい天気ですね今日はいい天気ですね今日はいい天気ですね今日はいい天気ですね',
-        goodNumber: '0',
-      ));
-      testData.add(TourouData(
-        profileImagePath: Assets.images.iconNoImage.path,
-        userName: 'kenta',
-        userId: 'kenta3456',
-        tourouText: '今日はいい天気ですね',
-        goodNumber: '5000',
-      ));
+    for (var i = 0; i < 25; i++) {
+      testData
+        ..add(
+          TourouData(
+            profileImagePath: Assets.images.iconNoImage.path,
+            userName: 'kenta',
+            userId: 'kenta1234',
+            tourouText: '今日はいい天気ですね',
+            goodNumber: '0',
+          ),
+        )
+        ..add(
+          TourouData(
+            profileImagePath: Assets.images.iconNoImage.path,
+            userName: 'kentakentakentakenta',
+            userId: 'kenta5678',
+            tourouText: '今日はいい天気ですね',
+            goodNumber: '1',
+          ),
+        )
+        ..add(
+          TourouData(
+            profileImagePath: Assets.images.iconNoImage.path,
+            userName: 'kenta',
+            userId: 'kenta9012',
+            tourouText:
+                '今日はいい天気ですね今日はいい天気ですね今日はいい天気ですね今日はいい天気ですね今日はいい天気ですね今日はいい天気ですね今日はいい天気ですね今日はいい天気ですね',
+            goodNumber: '0',
+          ),
+        )
+        ..add(
+          TourouData(
+            profileImagePath: Assets.images.iconNoImage.path,
+            userName: 'kenta',
+            userId: 'kenta3456',
+            tourouText: '今日はいい天気ですね',
+            goodNumber: '5000',
+          ),
+        );
     }
 
     return TourouTestData._(tourouTestData: testData);
   }
+
+  TourouTestData._({required this.tourouTestData});
+
+  List<TourouData> tourouTestData;
 }
