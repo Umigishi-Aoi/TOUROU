9bdf4ab787d0c1c517aadd0232bfdefcb0b4e92d
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-05-03 23:09:41 +0900

    implemetnt world tourou user repository

--- tourou/lib/domain/repository/world_tourou_repository_interface.dart
+++ tourou/lib/domain/repository/world_tourou_repository_interface.dart
@@ -1,3 +1,15 @@
+import 'package:tourou/domain/value_object/tourou_language/tourou_language.dart';
+
+import '../entity/tourou_entity.dart';
+
 abstract class WorldTourouRepositoryInterface {
-  
+  Future<List<TourouEntity>> getTourouOrderedByGood();
+
+  Future<List<TourouEntity>> getTourouOrderedBynew();
+
+  Future<void> deleteTourou(TourouEntity tourou);
+
+  Future<void> addTourou(TourouEntity tourou);
+
+  Future<void> changeLanguage(TourouLanguage language);
 }
