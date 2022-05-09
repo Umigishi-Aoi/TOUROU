516ced9e6b672e55c83f60f909b63702e67e7d3e
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-05-09 23:11:28 +0900

    rename file, add class, add implementing method

---
+++ tourou/interface/user_test_repository.dart
@@ -0,0 +1,51 @@
+import 'package:tourou/domain/repository/user_repository_interface.dart';
+
+class UserTestRepository implements UserRepositoryInterface{
+
+  @override
+  void appleSignIn() {
+    // TODO: implement appleSignIn
+  }
+
+  @override
+  void appleSignUp() {
+    // TODO: implement appleSignUp
+  }
+
+  @override
+  void deleteAccount(String id) {
+    // TODO: implement deleteAccount
+  }
+
+  @override
+  void editUser() {
+    // TODO: implement editUser
+  }
+
+  @override
+  void googleSignIn() {
+    // TODO: implement googleSignIn
+  }
+
+  @override
+  void googleSignUp() {
+    // TODO: implement googleSignUp
+  }
+
+  @override
+  bool isContainedUserId(String id) {
+    // TODO: implement isContainedUserId
+    throw UnimplementedError();
+  }
+
+  @override
+  void saveUser() {
+    // TODO: implement saveUser
+  }
+
+  @override
+  void signOut() {
+    // TODO: implement signOut
+  }
+  
+}
